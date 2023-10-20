9915f062be6ef49a7511167412da0a2ee0db8502
Luisa <luisa-yi-qing.yang@student.tuiasi.ro>
2023-10-20 11:25:22 +0300

    https://github.com/luisay11/Py.git

--- ex14.c.f.py
+++ ex14.c.f.py
@@ -6,6 +6,6 @@ nr = 25
 bitNumber = 3                         # 0000 0000 0001 1001 = 25
 rezultat = nr & ~(1 << bitNumber)     # 0000 0000 0000 1000 = 8 (1 << 3)
 print(rezultat)                       # compl fata de 2 a nr 8: ..1111 0111
-                                      # ~8 = - 9 (- 1001) 
+print(rezultat)                       # ~8 = - 9 (- 1001) 
                                       # 25 & -9 = 0001 1001 & 1111 0111 = 0001 0001 = 17
                                       #              25  &  compl 2 a lui -9 = 17
